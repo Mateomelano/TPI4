18058615c187bd5254485a16cbe0dfcd40f07ff2
lautaromartinez04 <lautarom403@gmail.com>
2024-06-21 12:00:44 -0300

    commmit todo andando manga de buenas personas los quiero mucho

--- controladores/home/home.js
+++ controladores/home/home.js
@@ -1,43 +1,15 @@
 import { usuariosServices } from "../../servicios/usuarios-servicios.js";
-import { ventasServices } from "../../servicios/ventas-servicios.js";
-import { productosServices } from "../../servicios/productos-servicios.js";
 import { destinosServices } from "../../servicios/destinos-servicios.js";
+import { paquetesServices } from "../../servicios/paquetes-servicios.js";
 
 const htmlHome = 
 ` <div class="row" >
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-info">
-            <div class="inner">
-            <h3 id="indVentas">150</h3>
-
-            <p>Ventas</p>
-            </div>
-            <div class="icon">
-                <i class="ion ion-bag"></i>
-            </div>
-            <a href="#/ventas" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
+    <!-- ./col -->
+    
     <!-- ./col -->
     <div class="col-lg-3 col-6">
         <!-- small box -->
-        <div class="small-box bg-success">
-            <div class="inner">
-            <h3 id="indSindespachar">53</h3>
-
-            <p>Sin despachar</p>
-            </div>
-            <div class="icon">
-            <i class="ion ion-stats-bars"></i>
-            </div>
-            <a href="#/ventas" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-        </div>
-    </div>
-    <!-- ./col -->
-    <div class="col-lg-3 col-6">
-        <!-- small box -->
-        <div class="small-box bg-warning">
+        <div class="small-box">
             <div class="inner">
             <h3 id="indUsuarios">44</h3>
 
@@ -52,7 +24,7 @@ const htmlHome =
 
     <div class="col-lg-3 col-6">
         <!-- small box -->
-        <div class="small-box bg-warning">
+        <div class="small-box">
             <div class="inner">
             <h3 id="indDestinos">44</h3>
 
@@ -65,19 +37,17 @@ const htmlHome =
         </div>
     </div>
 
-    <!-- ./col -->
     <div class="col-lg-3 col-6">
         <!-- small box -->
-        <div class="small-box bg-danger">
+        <div class="small-box">
             <div class="inner">
-            <h3 id="indProductos">65</h3>
-
-            <p>Productos</p>
+            <h3 id="indPaquetes">0</h3>
+            <p>Paquetes</p>
             </div>
             <div class="icon">
             <i class="ion ion-pie-graph"></i>
             </div>
-            <a href="#/productos" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="#/paquetes" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
         </div>
     </div>
     <!-- ./col -->
@@ -94,12 +64,9 @@ export async function Home(){
            
      
     cP.innerHTML =  htmlHome;
-     
-    let indVentas = d.getElementById ("indVentas");
-    let indSinDespachar = d.getElementById ("indSindespachar");
     let indUsuarios = d.getElementById ("indUsuarios");
-    let indProductos = d.getElementById ("indProductos");
     let indDestinos = d.getElementById ("indDestinos");
+    let indPaquetes = d.getElementById ("indPaquetes");
     //CANTIDAD DE USUARIOS
 
     res = await usuariosServices.listar();
@@ -107,18 +74,11 @@ export async function Home(){
     indUsuarios.innerHTML = res.length;
     
     //CANTIDAD DE VENTAS
-    res= await ventasServices.listar();
-    indVentas.innerHTML = res.length;
-
-    //CANTIDAD DE VENTAS SIN DESPACHAR (los valores que espera para el campo despachado son true y false)
-    res= await ventasServices.listarVentasDespachadas(false) ;
-    indSinDespachar.innerHTML = res.length;
-
-    //CANTIDAD DE PRODUCTOS
-    res= await productosServices.listar() ;
-    indProductos.innerHTML = res.length;
 
     res = await destinosServices.listar();
     indDestinos.innerHTML = res.length;
+
+    res = await paquetesServices.listar();
+    indPaquetes.innerHTML = res.length;
 
 }
